c4c337ac494abe875d4ec452872e8f7c29b8ee70
David A. Kennedy <me@davidakennedy.com>
2013-01-21 15:41:28 -0500

    Add custom header functionality to header.php.

--- header.php
+++ header.php
@@ -50,6 +50,13 @@
 			<h1 class="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</hgroup>
+		
+		<?php $header_image = get_header_image();
+	if ( ! empty( $header_image ) ) { ?>
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+			<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" />
+		</a>
+	<?php } // if ( ! empty( $header_image ) ) ?>
 
 	</header><!-- #masthead .site-header -->
 
