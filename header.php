e7f3a21fd3909dfbe02903a3cac6bfc458ea5dd7
David A. Kennedy <me@davidakennedy.com>
2013-03-06 20:38:02 -0500

    Adjust custom header stuff.

--- header.php
+++ header.php
@@ -54,13 +54,6 @@
 				<h1 class="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 			</hgroup>
-		
-			<?php $header_image = get_header_image();
-				if ( ! empty( $header_image ) ) { ?>
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-					<img class="banner-img"src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" />
-				</a>
-			<?php } // if ( ! empty( $header_image ) ) ?>
 		</div><!-- .site-banner -->
 	</header><!-- #masthead .site-header -->
 
--- inc/custom-header.php
+++ inc/custom-header.php
@@ -1,181 +1,202 @@
 <?php
 /**
- * Sample implementation of the Custom Header feature
- * http://codex.wordpress.org/Custom_Headers
+ * Implements a custom header for Zen Hacker.
+ * See http://codex.wordpress.org/Custom_Headers
  *
- * You can add an optional custom header image to header.php like so ...
-
-	<?php $header_image = get_header_image();
-	if ( ! empty( $header_image ) ) { ?>
-		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-			<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" />
-		</a>
-	<?php } // if ( ! empty( $header_image ) ) ?>
-
- *
- * @package ZH
- * @since ZH 1.0
+ * @package WordPress
+ * @subpackage ZH
+ * @since Zen Hacker 1.0
  */
 
 /**
- * Setup the WordPress core custom header feature.
+ * Sets up the WordPress core custom header arguments and settings.
  *
- * Use add_theme_support to register support for WordPress 3.4+
- * as well as provide backward compatibility for previous versions.
- * Use feature detection of wp_get_theme() which was introduced
- * in WordPress 3.4.
+ * @uses add_theme_support() to register support for 3.4 and up.
+ * @uses zh_header_style() to style front-end.
+ * @uses zh_admin_header_style() to style wp-admin form.
+ * @uses zh_admin_header_image() to add custom markup to wp-admin form.
+ * @uses register_default_headers() to set up the bundled header images.
  *
- * @uses zh_header_style()
- * @uses zh_admin_header_style()
- * @uses zh_admin_header_image()
- *
- * @package ZH
+ * @since Zen Hacker 1.0
  */
 function zh_custom_header_setup() {
 	$args = array(
-		'default-image'          => '',
+		// Text color and image (empty to use none).
 		'default-text-color'     => '54413b',
-		'width'                  => 768,
+		'default-image'          => '%s/img/headers/circle.png',
+
+		// Set height and width, with a maximum value for the width.
 		'height'                 => 200,
-		'flex-height'            => true,
+		'width'                  => 768,
+
+		// Callbacks for styling the header and the admin preview.
 		'wp-head-callback'       => 'zh_header_style',
 		'admin-head-callback'    => 'zh_admin_header_style',
 		'admin-preview-callback' => 'zh_admin_header_image',
 	);
 
-	$args = apply_filters( 'zh_custom_header_args', $args );
+	add_theme_support( 'custom-header', $args );
 
-	if ( function_exists( 'wp_get_theme' ) ) {
-		add_theme_support( 'custom-header', $args );
-	} else {
-		// Compat: Versions of WordPress prior to 3.4.
-		define( 'HEADER_TEXTCOLOR',    $args['default-text-color'] );
-		define( 'HEADER_IMAGE',        $args['default-image'] );
-		define( 'HEADER_IMAGE_WIDTH',  $args['width'] );
-		define( 'HEADER_IMAGE_HEIGHT', $args['height'] );
-		add_theme_support( 'custom-header', $args['wp-head-callback'], $args['admin-head-callback'], $args['admin-preview-callback'] );
-	}
+	/*
+	 * Default custom headers packaged with the theme.
+	 * %s is a placeholder for the theme template directory URI.
+	 */
+	register_default_headers( array(
+		'circle' => array(
+			'url'           => '%s/img/headers/circle.png',
+			'thumbnail_url' => '%s/img/headers/circle-thumbnail.png',
+			'description'   => _x( 'Circle', 'header image description', 'zh' )
+		),
+		'diamond' => array(
+			'url'           => '%s/img/headers/diamond.png',
+			'thumbnail_url' => '%s/img/headers/diamond-thumbnail.png',
+			'description'   => _x( 'Diamond', 'header image description', 'zh' )
+		),
+		'star' => array(
+			'url'           => '%s/img/headers/star.png',
+			'thumbnail_url' => '%s/img/headers/star-thumbnail.png',
+			'description'   => _x( 'Star', 'header image description', 'zh' )
+		),
+	) );
 }
 add_action( 'after_setup_theme', 'zh_custom_header_setup' );
 
 /**
- * Shiv for get_custom_header().
+ * Styles the header text displayed on the blog.
  *
- * get_custom_header() was introduced to WordPress
- * in version 3.4. To provide backward compatibility
- * with previous versions, we will define our own version
- * of this function.
+ * get_header_textcolor() options: Hide text (returns 'blank'), or any hex value.
  *
- * @return stdClass All properties represent attributes of the curent header image.
- *
- * @package ZH
- * @since ZH 1.1
- */
-
-if ( ! function_exists( 'get_custom_header' ) ) {
-	function get_custom_header() {
-		return (object) array(
-			'url'           => get_header_image(),
-			'thumbnail_url' => get_header_image(),
-			'width'         => HEADER_IMAGE_WIDTH,
-			'height'        => HEADER_IMAGE_HEIGHT,
-		);
-	}
-}
-
-if ( ! function_exists( 'zh_header_style' ) ) :
-/**
- * Styles the header image and text displayed on the blog
- *
- * @see zh_custom_header_setup().
- *
- * @since ZH 1.0
+ * @since Zen Hacker 1.0
  */
 function zh_header_style() {
+	$header_image = get_header_image();
+	$text_color   = get_header_textcolor();
 
-	// If no custom options for text are set, let's bail
-	// get_header_textcolor() options: HEADER_TEXTCOLOR is default, hide text (returns 'blank') or any hex value
-	if ( HEADER_TEXTCOLOR == get_header_textcolor() )
+	// If no custom options for text are set, let's bail.
+	if ( empty( $header_image ) && $text_color == get_theme_support( 'custom-header', 'default-text-color' ) )
 		return;
-	// If we get this far, we have custom styles. Let's do this.
+
+	// If we get this far, we have custom styles.
 	?>
 	<style type="text/css">
 	<?php
+		if ( ! empty( $header_image ) ) :
+	?>
+		.site-banner {
+			background: url("<?php header_image(); ?>") no-repeat center;
+			background-size: 48em auto;
+			min-height: 12.5em;
+		}
+	<?php
+		endif;
+
 		// Has the text been hidden?
-		if ( 'blank' == get_header_textcolor() ) :
+		if ( ! display_header_text() ) :
 	?>
 		.site-title,
 		.site-description {
 			position: absolute !important;
-			clip: rect(1px 1px 1px 1px); /* IE6, IE7 */
+			clip: rect(1px 1px 1px 1px); /* IE7 */
 			clip: rect(1px, 1px, 1px, 1px);
 		}
 	<?php
-		// If the user has set a custom color for the text use that
-		else :
+			if ( empty( $header_image ) ) :
+	?>
+		.site-header hgroup {
+			min-height: 0;
+		}
+	<?php
+			endif;
+
+		// If the user has set a custom color for the text, use that.
+		elseif ( $text_color != get_theme_support( 'custom-header', 'default-text-color' ) ) :
 	?>
 		.site-title a,
 		.site-description {
-			color: #<?php echo get_header_textcolor(); ?> !important;
+			color: #<?php echo esc_attr( $text_color ); ?> !important;
 		}
 	<?php endif; ?>
 	</style>
 	<?php
 }
-endif; // zh_header_style
 
-if ( ! function_exists( 'zh_admin_header_style' ) ) :
 /**
  * Styles the header image displayed on the Appearance > Header admin panel.
  *
- * @see zh_custom_header_setup().
- *
- * @since ZH 1.0
+ * @since Zen Hacker 1.0
  */
 function zh_admin_header_style() {
+	$header_image = get_header_image();
 ?>
 	<style type="text/css">
 	.appearance_page_custom-header #headimg {
 		border: none;
+		-webkit-box-sizing: border-box;
+		-moz-box-sizing:    border-box;
+		box-sizing:         border-box;
+		<?php
+		if ( ! empty( $header_image ) ) {
+			echo 'background: url("' . esc_url( $header_image ) . '") no-repeat scroll top; background-size: 48em auto;';
+		} ?>
+		padding: 0 2em;
 	}
+	#headimg .hgroup {
+		-webkit-box-sizing: border-box;
+		-moz-box-sizing:    border-box;
+		box-sizing:         border-box;
+		margin: 0 auto;
+		max-width: 48em;
+		<?php
+		if ( ! empty( $header_image ) || display_header_text() ) {
+			echo 'min-height: 200px;';
+		} ?>
+		text-align: center;
+		width: 100%;
+	}
+	<?php if ( ! display_header_text() ) : ?>
 	#headimg h1,
-	#desc {
+	#headimg h2 {
+		position: absolute !important;
+		clip: rect(1px 1px 1px 1px); /* IE7 */
+		clip: rect(1px, 1px, 1px, 1px);
 	}
+	<?php endif; ?>
 	#headimg h1 {
+		font: 46px/1.5 'Raleway', Tahoma, Arial, sans-serif;
+		margin: 0;
+		padding: 45px 0 0;
 	}
 	#headimg h1 a {
+		text-decoration: none;
 	}
-	#desc {
+	#headimg h1 a:hover {
+		text-decoration: underline;
 	}
-	#headimg img {
+	#headimg h2 {
+		font: 20px/1.5 'Raleway', Tahoma, Arial, sans-serif;
+		margin: 0;
+	}
+	.default-header img {
+		max-width: 48em;
+		width: auto;
 	}
 	</style>
 <?php
 }
-endif; // zh_admin_header_style
 
-if ( ! function_exists( 'zh_admin_header_image' ) ) :
 /**
- * Custom header image markup displayed on the Appearance > Header admin panel.
+ * Outputs markup to be displayed on the Appearance > Header admin panel.
+ * This callback overrides the default markup displayed there.
  *
- * @see zh_custom_header_setup().
- *
- * @since ZH 1.0
+ * @since Twenty Thirteen 1.0
  */
-function zh_admin_header_image() { ?>
-	<div id="headimg">
-		<?php
-		if ( 'blank' == get_header_textcolor() || '' == get_header_textcolor() )
-			$style = ' style="display:none;"';
-		else
-			$style = ' style="color:#' . get_header_textcolor() . ';"';
-		?>
-		<h1><a id="name"<?php echo $style; ?> onclick="return false;" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-		<div id="desc"<?php echo $style; ?>><?php bloginfo( 'description' ); ?></div>
-		<?php $header_image = get_header_image();
-		if ( ! empty( $header_image ) ) : ?>
-			<img src="<?php echo esc_url( $header_image ); ?>" alt="" />
-		<?php endif; ?>
+function zh_admin_header_image() {
+	?>
+	<div id="headimg" style="background: url('<?php esc_url( header_image() ); ?>') no-repeat scroll top; background-size: 768px auto;">
+		<?php $style = ' style="color:#' . get_header_textcolor() . ';"'; ?>
+		<div class="hgroup">
+			<h1><a id="name"<?php echo $style; ?> href="#"><?php bloginfo( 'name' ); ?></a></h1>
+			<h2 id="desc"<?php echo $style; ?>><?php bloginfo( 'description' ); ?></h2>
+		</div>
 	</div>
 <?php }
-endif; // zh_admin_header_image
