edc557989bef08b7622a3294e95f3319fa7c6660
David A. Kennedy <me@davidakennedy.com>
2013-04-16 22:31:34 -0400

    Buh bye hgroup.

--- header.php
+++ header.php
@@ -50,10 +50,8 @@
 	</div><!-- .skip-container -->
 	<header id="masthead" class="site-header cf" role="banner">
 		<div class="site-banner">
-			<hgroup>
 				<h1 class="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-			</hgroup>
 		</div><!-- .site-banner -->
 	</header><!-- #masthead .site-header -->
 
