e1d4731993674a54e9754cd29fa303bf15ba6c5c
David A. Kennedy <me@davidakennedy.com>
2015-12-14 15:32:14 -0500

    Remove support for `wp_title` for rendering title tag
    * This also fixes a bug in the theme.
    
    Fixes #70

--- inc/tweaks.php
+++ inc/tweaks.php
@@ -93,48 +93,6 @@ function accessiblezen_enhanced_image_navigation( $url, $id ) {
 }
 add_filter( 'attachment_link', 'accessiblezen_enhanced_image_navigation', 10, 2 );
 
-if ( version_compare( $GLOBALS['wp_version'], '4.1', '<' ) ) :
-	/**
-	 * Filters wp_title to print a neat <title> tag based on what is being viewed.
-	 *
-	 * @param string $title Default title text for current view.
-	 * @param string $sep Optional separator.
-	 * @return string The filtered title.
-	 */
-	function accessiblezen_wp_title( $title, $sep ) {
-		if ( is_feed() ) {
-			return $title;
-		}
-		global $page, $paged;
-		// Add the blog name
-		$title .= get_bloginfo( 'name', 'display' );
-		// Add the blog description for the home/front page.
-		$site_description = get_bloginfo( 'description', 'display' );
-		if ( $site_description && ( is_home() || is_front_page() ) ) {
-			$title .= " $sep $site_description";
-		}
-		// Add a page number if necessary:
-		if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() ) {
-			$title .= " $sep " . sprintf( __( 'Page %s', 'accessiblezen' ), max( $paged, $page ) );
-		}
-		return $title;
-	}
-	add_filter( 'wp_title', 'accessiblezen_wp_title', 10, 2 );
-	/**
-	 * Title shim for sites older than WordPress 4.1.
-	 *
-	 * @link https://make.wordpress.org/core/2014/10/29/title-tags-in-4-1/
-	 * @todo Remove this function when WordPress 4.3 is released.
-	 * @since accessiblezen 1.1.4
-	 */
-	function accessiblezen_render_title() {
-		?>
-		<title><?php wp_title( '|', true, 'right' ); ?></title>
-		<?php
-	}
-	add_action( 'wp_head', 'accessiblezen_render_title' );
-endif;
-
 if ( ! function_exists( 'accessiblezen_excerpt_length' ) ) :
 /**
  * Sets the post excerpt length to 100 words.
