ec24380d4fcbc976642e27fbb3b09c2f1d08b638
David A. Kennedy <me@davidakennedy.com>
2015-12-22 17:42:07 -0500

    Escape output in comments.php file
    Relates to #72

--- comments.php
+++ comments.php
@@ -27,8 +27,11 @@
 	<?php if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
-				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'accessible-zen' ),
-					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
+				printf( // WPCS: XSS OK.
+					esc_html( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'accessible-zen' ) ),
+					number_format_i18n( get_comments_number() ),
+					'<span>' . get_the_title() . '</span>'
+				);
 			?>
 		</h2>
 
@@ -47,14 +50,14 @@
 			if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) :
 		?>
 		<nav class="navigation comment-navigation cf" role="navigation">
-			<h2 class="screen-reader-text"><?php _e( 'Comment navigation', 'accessible-zen' ); ?></h2>
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'accessible-zen' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'accessible-zen' ) ); ?></div>
+			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'accessible-zen' ); ?></h2>
+			<div class="nav-previous"><?php previous_comments_link( esc_html__( '&larr; Older Comments', 'accessible-zen' ) ); ?></div>
+			<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments &rarr;', 'accessible-zen' ) ); ?></div>
 		</nav><!-- .comment-navigation -->
 		<?php endif; // Check for comment navigation ?>
 
 		<?php if ( ! comments_open() && get_comments_number() ) : ?>
-		<p class="no-comments"><?php _e( 'Comments are closed.' , 'accessible-zen' ); ?></p>
+		<p class="no-comments"><?php esc_html_e( 'Comments are closed.' , 'accessible-zen' ); ?></p>
 		<?php endif; ?>
 
 	<?php endif; // have_comments() ?>
