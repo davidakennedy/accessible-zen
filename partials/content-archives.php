9991016504fc0d23d1d208d48d927a7d5ba5001e
David A. Kennedy <me@davidakennedy.com>
2016-04-20 23:06:40 -0400

    Escape output in content-archives.php
    Relates to #72

--- partials/content-archives.php
+++ partials/content-archives.php
@@ -15,22 +15,22 @@
 	<div class="entry-content">
 		<?php the_content(); ?>
 		<div class="search-archives">
-			<p><?php _e( 'Try searching to find something.', 'accessible-zen' ); ?></p>
+			<p><?php esc_html_e( 'Try searching to find something.', 'accessible-zen' ); ?></p>
 			<?php get_search_form(); ?>
 		</div>
-		<h2><?php _e( 'Archives by Month', 'accessible-zen' ); ?></h2>
+		<h2><?php esc_html_e( 'Archives by Month', 'accessible-zen' ); ?></h2>
 		<ul>
 			<?php wp_get_archives('show_post_count=1'); ?>
 		</ul>
-		<h2><?php _e( 'Archives by Category', 'accessible-zen' ); ?></h2>
+		<h2><?php esc_html_e( 'Archives by Category', 'accessible-zen' ); ?></h2>
 		<ul>
 			 <?php wp_list_categories(); ?>
 		</ul>
-		<h2><?php _e( 'Archives by Tag', 'accessible-zen' ); ?></h2>
+		<h2><?php esc_html_e( 'Archives by Tag', 'accessible-zen' ); ?></h2>
 		<div class="tags">
 			<?php wp_tag_cloud('orderby=count'); ?>
 		</div>
-		<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'accessible-zen' ), 'after' => '</div>' ) ); ?>
-		<?php edit_post_link( __( 'Edit', 'accessible-zen' ), '<span class="edit-link cf">', '</span>' ); ?>
+		<?php wp_link_pages( array( 'before' => '<div class="page-link">' . esc_html__( 'Pages:', 'accessible-zen' ), 'after' => '</div>' ) ); ?>
+		<?php edit_post_link( esc_html__( 'Edit', 'accessible-zen' ), '<span class="edit-link cf">', '</span>' ); ?>
 	</div><!-- .entry-content -->
 </article><!-- #post-<?php the_ID(); ?> -->
