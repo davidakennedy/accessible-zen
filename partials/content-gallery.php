6a682d0502996294edb1a3831dd61c256a30835b
David A. Kennedy <me@davidakennedy.com>
2016-04-21 08:52:04 -0400

    Escape output in content-gallery.php
    * Also remove `echo` from `get_post_format_archive_link()`. Not needed.
    
    Relates to #72

--- partials/content-gallery.php
+++ partials/content-gallery.php
@@ -11,20 +11,26 @@
 		<div class="gallery">
 			<header class="entry-header">
 				<span class="title">
-					<?php _e( 'Gallery', 'accessible-zen' ); ?>
+					<?php esc_html_e( 'Gallery', 'accessible-zen' ); ?>
 				</span>
 				<h2 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 			</header>
 
 			<div class="entry-content">
-				<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'accessible-zen' ) ); ?>
+				<?php
+					the_content(
+						wp_kses( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'accessible-zen' ),
+						array( 'span' => array( 'class' => array() ) ) )
+					);
+				?>
 			</div><!-- .entry-content -->
 		</div><!-- .gallery -->
 
 		<footer class="entry-meta">
 			<?php accessiblezen_posted_on(); ?>
 			<span class="post-format-info">
-				<?php echo get_post_format_archive_link(); ?><?php get_post_format_icon(); ?>
+				<?php get_post_format_archive_link(); ?>
+				<?php get_post_format_icon(); ?>
 			</span>
 			<?php edit_post_link( __( 'Edit', 'accessible-zen' ), '<span class="edit-link">', '</span>' ); ?>
 		</footer><!-- .entry-meta -->
