ffe85ee3e52cb7131ff9b41f134d38336bc7ae97
David A. Kennedy <me@davidakennedy.com>
2016-04-20 23:36:47 -0400

    Escape output of content-aside.php
    Relates to #72

--- partials/content-aside.php
+++ partials/content-aside.php
@@ -11,20 +11,26 @@
 		<div class="aside">
 			<header class="entry-header">
 				<span class="title">
-					<?php _e( 'Aside', 'accessible-zen' ); ?>
+					<?php esc_html_e( 'Aside', 'accessible-zen' ); ?>
 				</span>
 				<h2 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 			</header>
 
 			<div class="entry-content">
-				<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'accessible-zen' ) ); ?>
+				<?php
+					the_content(
+						wp_kses( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'accessible-zen' ),
+						array( 'span' => array( 'class' => array() ) ) )
+					);
+				?>
 			</div><!-- .entry-content -->
 		</div><!-- .aside -->
 
 		<footer class="entry-meta">
 			<?php accessiblezen_posted_on(); ?>
 			<span class="post-format-info">
-				<?php echo get_post_format_archive_link(); ?><?php get_post_format_icon(); ?>
+				<?php echo get_post_format_archive_link(); // WPCS: XSS OK. ?>
+				<?php get_post_format_icon(); ?>
 			</span>
 			<?php edit_post_link( __( 'Edit', 'accessible-zen' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-meta -->
