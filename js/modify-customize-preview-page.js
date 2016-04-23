8aaeb63dd07825e137a5113c2d8bf2635377246f
David A. Kennedy <me@davidakennedy.com>
2016-04-23 01:10:14 -0400

    Show "Read More Posts" link if page is selected.
    * Applies to Customizer.
    * Works if a page is selected as front page.
    
    Fixes #74

--- js/modify-customize-preview-page.js
+++ js/modify-customize-preview-page.js
@@ -5,6 +5,10 @@
 jQuery( document ).ready(function( $ ) {
 	// Hide divs on Customizer panel by default
 	$( '#customize-control-show_more_posts_link' ).hide();
+	// But show it if it's already selected
+	if ( $( 'input[value="page"]' ).is( ':checked' ) ) {
+		$( '#customize-control-show_more_posts_link' ).show();
+	}
 	// Hide/Show divs on Customizer panel, based on selection
 	$( 'input[value="posts"]' ).change(function() {
 		$( '#customize-control-show_more_posts_link' ).hide();
