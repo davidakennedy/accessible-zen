89d82e8fa59b36e5c3f0c7c1f6037372554b1a3f
David A. Kennedy <me@davidakennedy.com>
2016-05-03 09:17:40 -0400

    Add `aria-labelledby` attribute to menus.
    * Provides a label to differentiate multiple navigation landmarks.
    
    Fixes #76

--- footer.php
+++ footer.php
@@ -14,16 +14,16 @@
 
 	<footer id="colophon" class="site-footer cf" role="contentinfo">
 		<?php if ( has_nav_menu( 'primary' ) ) : ?>
-			<nav role="navigation" class="main-navigation cf">
-				<h2 class="screen-reader-text"><?php esc_html_e( 'Main Menu', 'accessible-zen' ); ?></h2>
+			<nav role="navigation" class="main-navigation cf" aria-labelledby="main-menu-heading">
+				<h2 id="main-menu-heading" class="screen-reader-text"><?php esc_html_e( 'Main Menu', 'accessible-zen' ); ?></h2>
 
 				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav', 'depth' => 1 ) ); ?>
 			</nav><!-- .main-navigation -->
 		<?php endif; ?>
 
 		<?php if ( has_nav_menu( 'secondary' ) ) : ?>
-			<nav role="navigation" class="secondary-navigation cf">
-				<h2 class="screen-reader-text"><?php esc_html_e( 'Secondary Menu', 'accessible-zen' ); ?></h2>
+			<nav role="navigation" class="secondary-navigation cf" aria-labelledby="secondary-menu-heading">
+				<h2 id="secondary-menu-heading" class="screen-reader-text"><?php esc_html_e( 'Secondary Menu', 'accessible-zen' ); ?></h2>
 
 				<?php wp_nav_menu( array( 'theme_location' => 'secondary', 'menu_class' => 'nav', 'depth' => 1 ) ); ?>
 			</nav><!-- .secondary-navigation -->
